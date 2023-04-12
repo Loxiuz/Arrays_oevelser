3f64b7754a91e8edc971e257ea5b1bdf3ac3f146
Malte <maltebjoerklund@gmail.com>
2023-04-12 15:35:26 +0200

    Update script.js

--- counting_array/script.js
+++ counting_array/script.js
@@ -1,5 +1,18 @@
 "use strict";
 
-windows.addEventListener("load", start);
+window.addEventListener("load", start);
 
-function start() {}
+const array = [];
+let counter = 0;
+
+function start() {
+  setInterval(arrayCounter, 1000);
+}
+
+function arrayCounter() {
+  if (array.unshift(counter) == 10) {
+    array.pop();
+  }
+  console.log(array);
+  counter++;
+}
